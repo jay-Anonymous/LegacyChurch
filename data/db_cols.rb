18e74039faf09974a7d1dc0a55f757dcbd47b2a8
David Morrison <drmorr@evokewonder.com>
2015-11-04 11:22:43 -0800

    update to data parsing scripts

--- data/db_cols.rb
+++ data/db_cols.rb
@@ -3,6 +3,7 @@ $db_column_rules = {'MISSENGAGE' => [['MISPAR']],
 				    'CFCHILD'    => [['CSCHILD', 'AOCHILD']],
 				    'CFYOUTH'    => [['CSYOUTH', 'AOYOUTH']],
 				    'CFOADLT'    => [['CSADULTS', 'CSLEAD', 'AOADULTS', 'AOLEAD']],
+					'CFTOTAL'    => [['CSTOTAL', 'AOTOTAL']],
 				    'ONGOCLASS'  => [['CHOTH', 'YOOTH', 'YOADULT']],
 				    'OUTSRVD'    => [['TOTSERV']],
 				    'VALPROP'    => [['VALCHUR', 'VALPARS']],
@@ -12,62 +13,79 @@ $db_column_rules = {'MISSENGAGE' => [['MISPAR']],
 				    'TOTPAST'    => [['PASTHOUS', 'ASSCHOUS'], ['HOUSUTIL']],
 				    'TOTREMB'    => [['PASTREMB', 'ASSCREMB'], ['REIMBURS']],
 				    'TOTCASH'    => [['OTHCASHA', 'OTHCASHb'], ['OTHCASH']],
-				    'DIACCOMP'   => [['DEACOMP']],
+
+					# DCNCOMP counts deacons and DIACCOMP counts diaconal ministers; as of 2014, no
+					# more diaconal ministers (as per Adrian's email), so we will just combine them
+					# for the entire range
+				    'DEACOMP'    => [['DIACCOMP', 'DCNCOMP']],
+
+					# Before 2012, NUMBAPT was the only field: we still track NUMBAPT, but
+					# from 2012 on, the database will also break down child/adult
 				    'NUMBAPT'    => [['BAPTCHILD', 'BAPTADULT']],
 
-# Don't print warning messages about the following column headers
-					'BAPTCHILD'  => [],
-			        'BAPTADULT'  => [],
-		  		    'PENBILLED'  => [],
-				    'RECCOR'     => [],
-				    'REMCOR'     => [],
-				    'CFYADLT'    => [],
-				    'SHORTCLASS' => [],
-				    'VBSPART'    => [],			
-				    'CSCLASS'    => [], 			
-				    'DAYSRVD'    => [],			
-				    'UMDIRECT'   => [],			
-				    'HLTHBILLED' => [], 		
-				    'DCNCOMP'    => [],
-				    'RENTAL'     => [],			
-				    'MEMBMR'     => [],			
-				    'UMVIM'      => [],			
-				    'UMVIMPAR'   => [],			
-				    'GENCHROFa'  => [],			
-				    'GENCHROFb'  => [],			
-				    'GENCHROFc'  => [],			
-				    'GENCHROFd'  => [],			
-				    'GENCHROFe'  => [],			
-				    'GENCHROFf'  => [],			
-				    'NUMPLEDG'   => [],			
-				    'BENOTHAa'   => [],			
-				    'BENOTHAb'   => [],			
-				    'BENOTHAc'   => [],			
-				    'BENOTHAd'   => [],			
-				    'BENOTHAe'   => [],			
-				    'BENOTHAf'   => [],			
-				    'BENOTHAg'   => [],			
-				    'CAPFUNa'    => [],			
-				    'CAPFUNb'    => [],
-				    'CAPFUNc'    => [],
-				    'CAPFUNd'    => [],			
-				    'FUNDSCRa'   => [],
-				    'FUNDSCRb'   => [],
-				    'FUNDSCRc'   => [],	
-				    'BENOTHA'    => [],		
-				    'MEMBFEM'    => [],			
-				    'MEMBMALE'   => [],			
+					# Apportionments for 2000-2009, as per Adrian's email
+					'TOTAPP'     => [['WSA', 'CBA', 'MEFA', 'BCFA', 'AUFA', 'JKMA', 'ICFA', 'ADMGENA', 'ADMJURA', 'AMDCONFA', 'Hins', 'DSFUNDA', 'EPISFUNA'],
+					  				 ['WSA', 'CBA', 'MEFA', 'BCFA', 'AUFA', 'ICFA', 'ADMGENA', 'ADMJURA', 'ADMCONFA', 'ADMDISTA', 'ADMOPENA', 'PENTREAA', 'DSFUNDA', 'EPISFUNA', 'EQUCOMPA', 'CLGYOPA']],
+					'APPPAID'    => [['WS', 'CB', 'MF', 'BCF', 'AUF', 'JKMAP', 'ICF', 'ADMGEN', 'ADMJUR', 'ADMCONF', 'HINSP', 'DSFUND', 'EPISFUND'],
+					  			     ['WS', 'CB', 'MEF', 'BC', 'AUF', 'ICF', 'ADMGEN', 'ADMJURIS', 'ADMCONF', 'ADMDIST', 'ADMOPEN', 'PenTreas', 'DSFUND', 'EPISFUND', 'EQUCOMP', 'CLGYOPEN']],
+					'ACSPSUN'	 => [['ACSPSUNa', 'ACSPSUNb', 'ACSPSUNc', 'ACSPSUNd']], 
+					'GENCHROF'   => [['GENCHROFa', 'GENCHROFb', 'GENCHROFc', 'GENCHROFd', 'GENCHROFe', 'GENCHROFf']], 
  
-				    'CSATTSHT'   => [],			
-				    'UMYFMEMB'   => [],			
-				    'UMYFPROJ'   => [],			
-				    'UMWTREAS'   => [],			
+# Don't print warning messages about the following column headers
+					
+					'BAPTCHILD'  => [],			# not tracked before 2012
+			        'BAPTADULT'  => [],			# not tracked before 2012
 
+				    'RECCOR'     => [],			# not tracked before 2009
+				    'REMCOR'     => [],			# not tracked before 2009
+				    'CFYADLT'    => [],			# before 2009, combined with CSADULT/AOADULT
+				    'SHORTCLASS' => [],			# not tracked before 2009
+				    'VBSPART'    => [],			# not tracked before 2009
+				    'CSCLASS'    => [], 		# not tracked before 2009
+				    'DAYSRVD'    => [],			# not tracked before 2009
+				    'UMDIRECT'   => [],			# not tracked before 2009
+				    'RENTAL'     => [],			# not tracked before 2006
+				    'MEMBMR'     => [],			# not tracked before 2005
+				    'UMVIM'      => [],			# not tracked before 2005
+				    'UMVIMPAR'   => [],			# not tracked before 2005
+				    'NUMPLEDG'   => [],			# not tracked before 2005
+				    'BENOTHAa'   => [],			# not tracked before 2005
+				    'BENOTHAb'   => [],			# not tracked before 2005
+				    'BENOTHAc'   => [],			# not tracked before 2005
+				    'BENOTHAd'   => [],			# not tracked before 2005
+				    'BENOTHAe'   => [],			# not tracked before 2005
+				    'BENOTHAf'   => [],			# not tracked before 2005
+				    'BENOTHAg'   => [],			# not tracked before 2005
+					'CAPFUNa'    => [],			# not tracked before 2005
+				    'CAPFUNb'    => [],			# not tracked before 2005
+				    'CAPFUNc'    => [],			# not tracked before 2005
+				    'CAPFUNd'    => [],			# not tracked before 2009
+				    'FUNDSCRa'   => [],			# not tracked before 2005
+				    'FUNDSCRb'   => [],			# not tracked before 2005
+				    'FUNDSCRc'   => [],			# not tracked before 2005
+				    'BENOTHA'    => [],			# not tracked before 2002
+				    'MEMBFEM'    => [],			# not tracked before 2001
+				    'MEMBMALE'   => [],			# not tracked before 2001
 
+				    'CSATTSHT'   => [],			# not tracked after 2008
+				    'UMYFMEMB'   => [],			# not tracked after 2008
+				    'UMYFPROJ'   => [],			# not tracked after 2008
+				    'UMWTREAS'   => [],			# not tracked after 2007
+
+					# PENBILLED/HEALTHBILLED represent a change in practice over the years, as
+					# per Adrian's email.  For now we will ignore them until we get the broken-down
+					# data from other sources.
+		  		    'PENBILLED'  => [],
+				    'HLTHBILLED' => [], 		
+
+					# Not relevant, as per Adrian's email
+					'DISTAPPOR'  => [],
+					'DISTPAID'   => [],
 
 					'year'       => [],
-					'computed_values' => ['MEMBTOT', 'CFTOTAL', 'GRANDTOT', 'ANNOPP', 'CAPFUN', 'FUNDSCR'],
-					'ignored'    => ['church_id', 'name', 'district', 'city', 'state']
+					'ignored'    => ['church_id', 'name', 'district', 'city', 'state'],
+					'no_data'    => ['CVNTMEM', 'MISTEAMS', 'DAYCARE', 'OUTRCH'],
+					'handled'    => ['CFPTOT', 'CSONGO', 'CFGTOT'],
 }
 
 def check_xls_cols(xls_cols, db_cols)
