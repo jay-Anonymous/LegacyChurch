8055377f68a9ddc09f62e85e04480d2ea7e690d5
David Morrison <drmorr0@gmail.com>
2015-11-15 20:37:51 -0800

    histogram function working, needs cleanup

--- app/assets/javascripts/histogram.js
+++ app/assets/javascripts/histogram.js
@@ -35,11 +35,15 @@ function get_hist_thresholds(raw, index) {
 	return thresholds;
 }
 
-function show_histogram(church_data, index) {
+function show_histogram(church_data) {
 
 	var raw = JSON.parse(church_data);
-	var data = d3.layout.histogram()
-		.bins(get_hist_thresholds)(raw, index);
+	var keys = Object.keys(raw[0]);
+	var index = keys[keys.length - 1];
+	var data = d3.layout.histogram(raw, index)
+		.value(function(d) { return d[index]; })
+		.bins(get_hist_thresholds(raw, index));
+	var binnedData = data(raw);
 
 	var margin = {top: 15, right: 20, bottom: 30, left: 5},
 		width = 600 - margin.left - margin.right,
@@ -52,29 +56,29 @@ function show_histogram(church_data, index) {
 		.attr("transform", "translate(" + margin.left + "," + margin.top+ ")");
 
 	var x = d3.scale.linear()
-		.domain([data[0].x, data[data.length - 1].x + data[0].dx])
+		.domain([binnedData[0].x, binnedData[binnedData.length - 1].x + binnedData[0].dx])
 		.range([0, width])
 	var y = d3.scale.linear()
-		.domain([0, d3.max(data, function(d) { return d.y; })])
+		.domain([0, d3.max(binnedData, function(d) { return d.y; })])
 		.range([height, 0]);
 	var xAxis = d3.svg.axis()
 		.scale(x)
 		.orient("bottom")
-		.tickValues(d3.range(data[0].x, data[data.length - 1].x + data[0].dx, data[0].dx))
+		.tickValues(d3.range(binnedData[0].x, binnedData[binnedData.length - 1].x + binnedData[0].dx, binnedData[0].dx))
 		.tickFormat(d3.format(",.0f"))
 
 	var bar = chart.selectAll("g")
-	  .data(data)
+	  .data(binnedData)
 	  .enter().append("g")
 	  .attr("transform", function(d, i) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
 	bar.append("rect")
 	  .attr("x", 1)
-	  .attr("width", x(data[0].dx) - 1)
+	  .attr("width", x(binnedData[0].dx) - 1)
 	  .attr("height", function(d) { return height - y(d.y); })
 
 	bar.append("text")
-	  .attr("x", x(data[0].dx) / 2)
+	  .attr("x", x(binnedData[0].dx) / 2)
 	  .attr("y", -10)
 	  .attr("dy", ".75em")
 	  .text(function(d) { return d.y; });
