a3edecb3700175a132a99c4ef867c1abee9f294f
David Morrison <drmorr0@gmail.com>
2015-12-07 00:14:09 -0800

    db corrections

--- data/db_corrections.rb
+++ data/db_corrections.rb
@@ -9,6 +9,17 @@ db_cols = pst.columns
 
 # Include manual corrections here:
 
+puts "Swap data in 2005 for 883520 and 937007? [y/n]"
+response = STDIN.gets.chomp
+if response[0] == 'y' then
+	sql = 'update church_data set church_id=111111 where church_id=883520 and year=2005'
+	db.execute sql;
+	sql = 'update church_data set church_id=883520 where church_id=937007 and year=2005'
+	db.execute sql;
+	sql = 'update church_data set church_id=937007 where church_id=111111 and year=2005'
+	db.execute sql;
+end
+
 puts "Done!"
 
 
